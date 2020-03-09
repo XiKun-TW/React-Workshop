cf5ff6df1150d089eef3db2e0a245701e81d6b7b
Gougougogogo <394442302@qq.com>
2020-03-09 13:16:25 +0800

    Setup form and input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const handleFormSubmit : (e : React.FormEvent<HTMLFormElement>) => void = e => {
+    if(e.target) {
+      // @ts-ignore
+      const firstName = e.target['first-name'].value;
+      // @ts-ignore
+      const lastName = e.target['last-name'].value;
+
+      alert(`User Name: ${firstName} ${lastName}`);
+    }
+    e.preventDefault();
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        Kun Xi's React Demo
-      </header>
-      <main className="App-body">
+    <div className="app">
+      <header className="app-header">Kun Xi's React Demo</header>
+      <main className="app-body">
+        <h2 className="body-title">Personal Info</h2>
+        <form className="input-form" onSubmit={handleFormSubmit}>
+          <label className="input-label" htmlFor="first-name">First Name:</label>
+          <input className="text-input" id="first-name" />
+          <label className="input-label" htmlFor="last-name">Last Name:</label>
+          <input className="text-input" id="last-name" />
+          <button className="primary-button">submit</button>
+        </form>
       </main>
     </div>
   );
