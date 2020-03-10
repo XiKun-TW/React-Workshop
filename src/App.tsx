6e4430cc7498ee159d51f4070ed913b3d0a30eed
Gougougogogo <394442302@qq.com>
2020-03-10 14:52:29 +0800

    Add form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,104 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Select, { SelectItem } from "./components/select";
 import { useProvinceCitys } from "./components/customHooks/useProvinceCitys";
 import { useMultiSelections } from "./components/customHooks/useMultiSelections";
 import { useSingleSelection } from "./components/customHooks/useSingleSelection";
-import { DEFAULT_GRADE_ITEMS, DEFAULT_SKILL_ITEMS} from './constants/mockData';
-import { PROVINCE_META_DATA, ProvinceName } from './constants/citys';
+import { DEFAULT_GRADE_ITEMS, DEFAULT_SKILL_ITEMS } from "./constants/mockData";
+import { PROVINCE_META_DATA, ProvinceName } from "./constants/citys";
 import "./App.css";
 
 function App() {
-  const provinces : SelectItem[] = PROVINCE_META_DATA.map(province => ({ id: province.id, value: province.name, isSelected: false }));
-  const [gradeItems, setGradeSelection] = useSingleSelection(DEFAULT_GRADE_ITEMS);
-  const [skillItems, setSkillSelection] = useMultiSelections(DEFAULT_SKILL_ITEMS);
-  const [provinceSelections, setProvinceSelection] = useSingleSelection(provinces);
-  const [citys, setProvince] = useProvinceCitys('');
-  const citySelection : SelectItem[] = citys.map(city => ({ id: city.id, value: city.name, isSelected: false }));
-  const [citySelections, setCitySelection, resetSelections] = useSingleSelection(citySelection);
- 
+  const provinces: SelectItem[] = PROVINCE_META_DATA.map(province => ({
+    id: province.id,
+    value: province.name,
+    isSelected: false
+  }));
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isFemale, setGender] = useState(false);
+  const formInput = useRef(null);
+  const [gradeItems, setGradeSelection] = useSingleSelection(
+    DEFAULT_GRADE_ITEMS
+  );
+  const [skillItems, setSkillSelection] = useMultiSelections(
+    DEFAULT_SKILL_ITEMS
+  );
+  const [provinceSelections, setProvinceSelection] = useSingleSelection(
+    provinces
+  );
+  const [citys, setProvince] = useProvinceCitys("");
+  const citySelection: SelectItem[] = citys.map(city => ({
+    id: city.id,
+    value: city.name,
+    isSelected: false
+  }));
+  const [
+    citySelections,
+    setCitySelection,
+    resetSelections
+  ] = useSingleSelection(citySelection);
+
+  const [isValid, setValidState] = useState(false);
+
+  // handle province and city interactive
   useEffect(() => {
-    if(!citys.every(city => citySelections.find(citySelection => citySelection.id === city.id))) {
-      const newCitySelection : SelectItem[] = citys.map(city => ({ id: city.id, value: city.name, isSelected: false }));
+    if (
+      !citys.every(city =>
+        citySelections.find(citySelection => citySelection.id === city.id)
+      )
+    ) {
+      const newCitySelection: SelectItem[] = citys.map(city => ({
+        id: city.id,
+        value: city.name,
+        isSelected: false
+      }));
       resetSelections(newCitySelection);
     }
-  }, [citys, citySelections, resetSelections])
+  }, [citys, citySelections, resetSelections]);
 
-  const handleProvinceSelectionChange = (selectedProvince : SelectItem) => {
+  // handle submit button status
+  useEffect(() => {
+    const hasSelection = (items: SelectItem[]) =>
+      items.some(item => item.isSelected);
+    setValidState(
+      !!firstName &&
+        !!lastName &&
+        hasSelection(gradeItems) &&
+        hasSelection(skillItems) &&
+        hasSelection(provinceSelections) &&
+        hasSelection(citySelections)
+    );
+  }, [
+    firstName,
+    lastName,
+    gradeItems,
+    skillItems,
+    provinceSelections,
+    citySelections
+  ]);
+
+  const handleProvinceSelectionChange = (selectedProvince: SelectItem) => {
     setProvince(selectedProvince.value as ProvinceName);
     setProvinceSelection(selectedProvince);
-  }
+  };
 
   const handleFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void = e => {
-    if (e.target) {
+    if (e.target && isValid) {
       // @ts-ignore
       const firstName = e.target["first-name"].value;
       // @ts-ignore
       const lastName = e.target["last-name"].value;
 
       alert(`User Name: ${firstName} ${lastName}`);
+    } else {
+      e.preventDefault();
     }
-    // e.preventDefault();
+  };
+
+  const handleGenderChanged = () => {
+    // @ts-ignore
+    const gender = formInput.current["gender"].value;
+    setGender(gender === "female");
   };
 
   return (
@@ -45,24 +106,54 @@ function App() {
       <header className="app-header">Kun Xi's React Demo</header>
       <main className="app-body">
         <h2 className="body-title">Personal Info</h2>
-        <form className="input-form" onSubmit={handleFormSubmit}>
+        <form
+          className="input-form"
+          onSubmit={handleFormSubmit}
+          ref={formInput}
+        >
           <label className="input-label" htmlFor="first-name">
             First Name:
           </label>
-          <input className="text-input" name="firstName" id="first-name" />
+          <input
+            className="text-input"
+            name="firstName"
+            value={firstName}
+            id="first-name"
+            onInput={e => setFirstName(e.currentTarget.value)}
+          />
           <label className="input-label" htmlFor="last-name">
             Last Name:
           </label>
-          <input className="text-input" name="lastName" id="last-name" />
+          <input
+            className="text-input"
+            value={lastName}
+            name="lastName"
+            id="last-name"
+            onInput={e => setLastName(e.currentTarget.value)}
+          />
           <fieldset className="filedset">
             <legend className="filedset-legend">Gender:</legend>
             <label className="radio-label" htmlFor="gender-male">
-              <input type="radio" value="male" name="gender" id="gender-male" />
+              <input
+                onChange={handleGenderChanged}
+                type="radio"
+                checked={!isFemale}
+                value="male"
+                name="gender"
+                id="gender-male"
+              />
               Male
             </label>
 
             <label className="radio-label" htmlFor="gender-female">
-              <input type="radio" value="female" name="gender" id="gender-female" />
+              <input
+                onChange={handleGenderChanged}
+                checked={isFemale}
+                type="radio"
+                value="female"
+                name="gender"
+                id="gender-female"
+              />
               Female
             </label>
           </fieldset>
@@ -95,7 +186,9 @@ function App() {
             isMultiple
             onItemClicked={setSkillSelection}
           />
-          <button className="primary-button">submit</button>
+          <button className={`primary-button ${isValid ? "" : "is-disabled"}`}>
+            submit
+          </button>
         </form>
       </main>
     </div>
