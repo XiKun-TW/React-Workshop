79756aea4f6bae546197140e32aba4557172f230
Gougougogogo <394442302@qq.com>
2020-03-31 13:22:29 +0800

    refactor for week 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
-  const handleFormSubmit : (e : React.FormEvent<HTMLFormElement>) => void = e => {
-    if(e.target) {
-      // @ts-ignore
-      const firstName = e.target['first-name'].value;
-      // @ts-ignore
-      const lastName = e.target['last-name'].value;
+type AppState = {
+  firstName: string;
+  lastName: string;
+};
 
+type AppProps = {};
+
+class App extends Component<AppProps, AppState> {
+  state = {
+    firstName: "",
+    lastName: ""
+  };
+
+  handleFormSubmit: (e: React.FormEvent<HTMLFormElement>) => void = e => {
+    if (e.target) {
+      const { firstName, lastName } = this.state;
       alert(`User Name: ${firstName} ${lastName}`);
     }
+
     e.preventDefault();
+  };
+
+  handleNameChange: (
+    inputName: "firstName" | "lastName",
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => void = (inputName, event) => {
+    this.setState({ [inputName]: event.currentTarget.value } as AppState);
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">Kun Xi's React Demo</header>
+        <main className="app-body">
+          <h2 className="body-title">Personal Info</h2>
+          <form className="input-form" onSubmit={this.handleFormSubmit}>
+            <InputField
+              labelName="First Name:"
+              name="firstName"
+              id="first-name"
+              value={this.state.firstName}
+              onChange={e => {
+                this.handleNameChange("firstName", e);
+              }}
+            />
+            <InputField
+              labelName="Last Name:"
+              name="lastName"
+              id="last-name"
+              value={this.state.lastName}
+              onChange={e => {
+                this.handleNameChange("lastName", e);
+              }}
+            />
+            <button className="primary-button">submit</button>
+          </form>
+        </main>
+      </div>
+    );
   }
-  
-  return (
-    <div className="app">
-      <header className="app-header">Kun Xi's React Demo</header>
-      <main className="app-body">
-        <h2 className="body-title">Personal Info</h2>
-        <form className="input-form" onSubmit={handleFormSubmit}>
-          <label className="input-label" htmlFor="first-name">First Name:</label>
-          <input className="text-input" id="first-name" />
-          <label className="input-label" htmlFor="last-name">Last Name:</label>
-          <input className="text-input" id="last-name" />
-          <button className="primary-button">submit</button>
-        </form>
-      </main>
-    </div>
-  );
 }
 
 export default App;
---
+++ src/components/InputField/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+
+type InputFieldProps = {
+  labelName: string;
+  id: string;
+  value: string;
+  name: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const InputField = (props: InputFieldProps) => {
+  const { labelName, ...inputProps } = props;
+  return (
+    <>
+      <label className="input-label" htmlFor={inputProps.id}>
+        {labelName}
+      </label>
+      <input className="text-input" {...inputProps} />
+    </>
+  );
+};
+
+export default InputField as React.FC<InputFieldProps>;
